7f87d15c558dd8478cafa2e47e791348cbf81d52
Prince XXIV <91701011+princevvs@users.noreply.github.com>
2021-11-18 23:50:30 +0000

    +

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-import random
+#import os
+#import random
 
 #Important Turtle Module 
 import turtle
@@ -26,13 +26,43 @@ class Sprite(turtle.Turtle):
         self.goto(startx, starty)
         self.speed = 1
 
+    def move(self):
+        self.fd(self.speed)
+
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 4
+        self.lives = 3
+
+    def turn_left(self):
+        self.lt(45)
+
+    def turn_right(self):
+        self.rt(45)
+
+    def accelerate(self):
+        self.speed += 1
+
+    def decelerate(self):
+        self.speed -=1
+
+
 
 #Create my Sprites 
-player = Sprite("triangle", "white", 0, 0)
+player = Player("triangle", "white", 0, 0)
+
+#Keyboard binding 
+turtle.onkey(player.turn_left, "Left")
+turtle.onkey(player.turn_right, "Right")
+turtle.onkey(player.turn_accelerate, "Up")
+turtle.onkey(player.turn_decelerate, "Down")
+turtle.listen()
+
+# Main Game loop 
+while True: 
+    player.move()
 
 
 
-
-
-
-delay = raw_input("Please enter to finish. >")
+#delay = raw_input("Please enter to finish. >")
