645d8af9ebed3b17ef201737f0f5d0d8756bf40e
Prince XXIV <91701011+princevvs@users.noreply.github.com>
2021-11-20 17:31:55 +0000

    Created a border

--- main.py
+++ main.py
@@ -29,6 +29,23 @@ class Sprite(turtle.Turtle):
     def move(self):
         self.fd(self.speed)
 
+        #Boundary detection 
+        if self.xcor() > 290:
+            self.setx(290)
+            self.rt(60)
+
+        if self.xcor() < -290:
+            self.setx(-290)
+            self.rt(60)
+
+        if self.ycor() > 290:
+            self.sety(290)
+            self.rt(60)
+
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.rt(60)            
+
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
         Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -47,6 +64,34 @@ class Player(Sprite):
     def decelerate(self):
         self.speed -=1
 
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+
+    def draw_border(self):
+        #Draw border 
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300, 300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+
+
+#Create game object 
+game = Game()
+
+#Draw he game border 
+game.draw_border()
 
 
 #Create my Sprites 
@@ -55,8 +100,8 @@ player = Player("triangle", "white", 0, 0)
 #Keyboard binding 
 turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
-turtle.onkey(player.turn_accelerate, "Up")
-turtle.onkey(player.turn_decelerate, "Down")
+turtle.onkey(player.accelerate, "Up")
+turtle.onkey(player.decelerate, "Down")
 turtle.listen()
 
 # Main Game loop 
@@ -65,4 +110,4 @@ while True:
 
 
 
-#delay = raw_input("Please enter to finish. >")
+delay = raw_input("Please enter to finish. >")
